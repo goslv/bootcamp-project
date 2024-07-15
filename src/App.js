e5ac9551fa5a4819f129961db9b4310a7d855a1a
goslv <frenzengamer@gmail.com>
2024-07-14 21:15:26 -0400

    Proyecto agregado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import './App.css';
-import Home from './pages/Home.jsx';
-import Contact from './pages/Contact.jsx';
-import About from './pages/About.jsx';
-import Page from './pages/Page.jsx';
-import Navbar from './components/NavBar.jsx';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import login from './components/login.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="flex flex-col">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/aboutus" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/page" element={<Page />} />
-        </Routes>
-      </Router>
+    <div className="flex">
+      <Navbar />
+      <div className="flex-grow">
+        <Router>
+          <Routes>
+            <Route path="/login" element={<login/>} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
---
+++ src/components/login.jsx
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/login.css"
+
+export default function login(){
+    const [email, setEmail]=useState('');
+    const [password, setPassword]=useState('');
+    const navigate = useNavigate();
+
+    const handleSumbit = (event)=>{
+        event.preventDefault();
+        console.log('Email',email);
+        console.log('Password',password);
+        navigate("/calc")
+    };
+    return(
+        <div className="container">
+            <form className="login" onSubmit={handleSumbit}>
+                <h2>Login</h2>
+                <div className="group">
+                    <label htmlFor="email">Email:</label>
+                    <input type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
+                    required
+                    />
+                    <label htmlFor="password">Contraseña:</label>
+                    <input type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
+                    required
+                    />
+                </div>
+            </form>
+        </div>
+    )
+}
